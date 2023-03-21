a2ded738dc3c39e91be80e315b65b4d1dcc0d6f3
Code with Dang <102164139+dungthinhtv@users.noreply.github.com>
2023-03-21 06:51:27 +0000

    asdas

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Sala town',
+    'salary': '2000$'
+  },
+  {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'Sala town',
+    'salary': '2000$'
+  },
+  {
+    'id': 3,
+    'title': 'Data Analyst',
+    'location': 'Sala town',
+    'salary': '2000$'
+  },
+]
 
 
 @app.route('/')
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route('/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
