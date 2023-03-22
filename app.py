3d23d54bea9d13db607d2b3f9edb9d99623e3344
dungthinhtv <dungthinhtv@gmail.com>
2023-03-22 20:50:36 +0700

    add jobpage

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+# from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 import os
 
@@ -27,16 +28,31 @@ JOBS = [
   },
 ]
 
+def find(arr , id):
+    for x in arr:
+        if x["id"] == id:
+            return x
+
 
 @app.route('/')
 def hello_world():
   return render_template('home.html', jobs=JOBS, app_name=APP_NAME)
 
 
-@app.route('/jobs')
+@app.route('/api/jobs')
 def list_jobs():
   return jsonify(JOBS)
 
+@app.route("/job/<id>")
+def show_job(id):  
+  job = find(JOBS , id)
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', 
+                         job=job)
+
+  
 
 if __name__ == '__main__':
   app.run(debug=True)
